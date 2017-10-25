af327fede68e20d8d609b13bd02621db535e4110
Allemel <allemel@list.ru>
2017-10-25 23:34:20 +0300

    lab3

---
+++ Lab3/3/3/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*Создать класс типа - Прямоугольник. Поля - высота и
+ширина. Методы вычисления площади, периметра,
+Свойства должны проверять корректность задаваемых
+параметров.
+Создать массив объектов. Вывести:
+a) количество четырехугольников разного типа (квадрат,
+прямоугольник, ромб, произвольный)
+b) определить для каждой группы наибольший и наименьший
+по площади (периметру) объект.*/
+namespace _3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var square = new Rectanglick(5, 5, "square");
+            var rect = new Rectanglick(5, 10, "rect");
+            var romb = new Rectanglick(7, 7, "romb");
+            var rand = new Rectanglick(5.7, 6.3, "rand");
+
+
+            Rectanglick[] arr = new Rectanglick[] {square, rect,  romb, rand};
+            Console.WriteLine("Rects: " + Rectanglick.RectCount.ToString() + " Squares: " + Rectanglick.SquareCount.ToString()  + 
+                " Rombs: " + Rectanglick.RombCount.ToString() +  " Rands: " + Rectanglick.RandCount.ToString());
+
+            double rectSMin = 10000;
+            double rectSMax = 1;
+            double randSMin = 10000;
+            double randSMax = 1;
+            double squareSMin = 10000;
+            double squareSMax = 1;
+            double rombSMin = 10000;
+            double rombSMax = 1;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                
+
+                switch (arr[i].Type)
+                {
+                    case "square":
+                        var s = arr[i].Square();
+                        if (s > squareSMax)
+                            squareSMax = s;
+                        if (s < squareSMin)
+                            squareSMin = s;
+                        break;
+                    case "rect":
+                        s = arr[i].Square();
+                        if (s > rectSMax)
+                            rectSMax = s;
+                        if (s < rectSMin)
+                            rectSMin = s;
+                        break;
+                    case "rand":
+                        s = arr[i].Square();
+                        if (s > randSMax)
+                            randSMax= s;
+                        if (s < randSMin)
+                            randSMin = s;
+                        break;
+                    case "romb":
+                        s = arr[i].Square();
+                        if (s > rombSMax)
+                            rombSMax = s;
+                        if (s < rombSMin)
+                            rombSMin = s;
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+            Console.WriteLine("Rect: Max: " + rectSMax.ToString()  + " Min: " + rectSMin.ToString());
+            Console.WriteLine("Rand: Max: " + randSMax.ToString()  + " Min: " + randSMin.ToString());
+            Console.WriteLine("Romb: Max: " + rombSMax.ToString()  + " Min: " + rombSMin.ToString());
+            Console.WriteLine("Square: Max: " + squareSMax.ToString()  + " Min: " + squareSMin.ToString());
+
+
+            var a = new Rectanglick(5.7, 6.3);
+            var v = new Rectanglick(5.7, 6.3);//стат члены
+            var l = new Rectanglick(2);
+            int la = 15;
+            int ju2;
+            Rectanglick.Tik(ref la, out ju2);
+            var t = a.ToString();
+ 
+
+
+
+            var n = " ";
+        }
+
+    }
+}
---
+++ Lab3/3/3/Rectanglick.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3
+{
+    class Rectanglick
+    {
+        static int count;//стат поле
+        public static int RectCount;
+        public static int SquareCount;
+        public static int RombCount;
+        public static int RandCount;
+
+
+        const int IKI = 18;
+        readonly double id;//поле для чтения 
+        double _h;
+        double _w;
+        public string Type;
+        public double Height
+        {
+            get { return _h; }
+            set
+            {
+                if (value > 0)
+                {
+                    _h = value;
+                }
+            }
+        }
+        public double Width
+        {
+            get { return _w; }
+            set
+            {
+                if (value > 0)
+                {
+                    _w = value;
+                }
+            }
+        }
+
+        static double Lol;
+        private int _i;
+
+        public int I//геттеры сеттеры для поля
+        {
+            get { return _i; }
+            set { _i = value; }
+        }
+
+
+        public Rectanglick(double h, double w, string t = "")//с параметрами
+        {
+            count++;
+            switch (t)
+            {
+                case "rect":
+                    RectCount++;
+                    break;
+                case  "romb":
+                    RombCount++;
+                    break;
+                case "square":
+                    SquareCount++;
+                    break;
+                case "rand":
+                    RandCount++;
+                    break;
+                default:
+                    break;
+            }
+
+            Height = h;
+            Width = w;
+            Type = t;
+            id = (h * w) - (IKI + 32);
+        }
+        public Rectanglick()//без  параметров
+        {
+        }
+
+        public Rectanglick(int i, double h = 4.6)//по умолчанию
+        {
+            Height = h;
+            Width = 10;
+
+        }
+        static  Rectanglick()//стат.конструктор (вызывается при первом обр к классу
+        {
+            Lol = 9;
+        }
+        private Rectanglick(int y)//закрытый
+        {
+        }
+
+        static public void Tik(ref int i, out int ju)//обл видимости (ток к глоб переменным)
+        {
+            ju = 8;//не иниц 
+            i += 10;//передача значения,будет выход в+10
+        }
+        static public string Metodinf()
+        {
+            return "Welcome to BelSTU";// метод возвращ строку
+        }
+        public override string ToString()//ovr -переопределить метод родителя
+        {
+            return "H: " + Height.ToString() + " W: " + Width.ToString();//строка+строка значени
+        }
+        public double Perimetr()
+        {
+            return Height * 2 + Width * 2;
+        }
+        public double Square()
+        {
+            return Height * Width;
+        }
+    }
+}
