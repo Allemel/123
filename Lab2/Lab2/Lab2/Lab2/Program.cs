2bf4edbd23417166286c8628a31d0ed8c6d0be07
Allemel <allemel@list.ru>
2017-10-25 23:33:17 +0300

    Lab2 added

--- Lab2/Lab2/Lab2/Lab2/Program.cs
+++ Lab2/Lab2/Lab2/Lab2/Program.cs
@@ -1,26 +1,26 @@
-﻿using System;//директива using сообщает компилятору где искать классы
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lab2//определение пространства имен
+namespace Lab2
 {
-    class Program//объявление класса    
+    class Program
     {
-        static void Main(string[] args)//статическая функция с возвращ значением
-        {//тело функции
-            string s = "";//явные преобразование
+        static void Main(string[] args)
+        {
+            string s = "";
             int i = 0;
             bool b = false;
             float f = 0;
             char c = 'd';
             uint ui = 2;
 
-            long l = (long)i;//неявные преобразования(табл.неявн.преобр.)
+            long l = (long)i;
             double d = (float)f;
             i = (int)c;
-            decimal dd = (decimal)i;//128-разр тип данных
+            decimal dd = (decimal)i;
             l = (long)ui;
 
             i = Convert.ToInt32("25");
@@ -29,37 +29,36 @@ namespace Lab2//определение пространства имен
             int ii = Convert.ToInt32(true);
             s = ui.ToString();
 
-            Object o;//упаковка 
+            Object o;
             o = ii;
-            int gg = (int)o;//распаковка
+            int gg = (int)o;
 
-            var bb = "sss";//отличие ссылочных типов от типов значений - перем.ссыл.типом могут принимать нал.
+            var bb = "sss";
             Object ob = null;
 
-            string s1 = "123";//объявление строковых литералов и создание строк на основе стринг
+            string s1 = "123";
             string s2 = "456";
             string s3 = "7 8 9";
-            if (s1 == s2)//сравнение
+            if (s1 == s2)
             {
                 Console.WriteLine("Equal");
             }
 
-            var res = String.Concat(s1, s2, s3);//сцепление
-            res = String.Copy(s1);//копирование
-            res = s1.Substring(1);//выделение подстроки
-            var res2 = s3.Split(' ');//разделение строки на слова
-            var res4 = s3.Insert(0, s2);//вставляет строку 2
-            res = s2.Replace("4", "");//перестановка подстроки в указ место
+            var res = String.Concat(s1, s2, s3);
+            res = String.Copy(s1);
+            res = s1.Substring(1);
+            var res2 = s3.Split(' ');
+            res = s2.Replace("4", "");
             string s4 = "";
-            string s5 = null;//nullстрока
+            string s5 = null;
             if (String.IsNullOrEmpty(s4))
             {
-                Console.WriteLine("Empty");//яв-ся ли строка 5 пустой или налл
+                Console.WriteLine("Empty");
             }
-            var sb = new StringBuilder("124");//строка на стр.билдер (позволяет менять строковую переменную)
-            sb.Remove(0, 1);//удаление символов
-            sb.Insert(0, "44");//вставка
-            float[,] arr = new float[,] { { 1, 2, 3 }, { 4, 5, 6 } };//оддномерн массив строк
+            var sb = new StringBuilder("124");
+            sb.Remove(0, 1);
+            sb.Insert(0, "44");
+            float[,] arr = new float[,] { { 1, 2, 3 }, { 4, 5, 6 } };
             Random ran = new Random();
             for (int iq = 0; i < 2; iq++)
             {
@@ -71,7 +70,7 @@ namespace Lab2//определение пространства имен
                 Console.WriteLine();
             }
 
-            string[] str = new string[] { "111", "222", "333" };//одномерн массив строк
+            string[] str = new string[] { "111", "222", "333" };
             for (int iq = 0; iq < str.Length; iq++)
             {
                 Console.Write(str[iq] + "\t");
@@ -81,7 +80,7 @@ namespace Lab2//определение пространства имен
             if (pos > str.Length)
             {
                 return;
-            }//ввод значчения - 
+            }
             string e = Console.ReadLine();
             str[pos-1] = e;
             for (int iq = 0; iq < str.Length; iq++)
@@ -91,7 +90,7 @@ namespace Lab2//определение пространства имен
             Console.WriteLine();
 
 
-            double[,] db = new double[3,4];//ступенч массив
+            double[,] db = new double[3,4];
             int tmp = 2;
             for (int iq = 0; iq < 3; iq++)
             {
@@ -115,11 +114,11 @@ namespace Lab2//определение пространства имен
                 Console.WriteLine();
             }
 
-            var bbb = new string[] { "123", "sss" };//неявно типиз переменные для хран массива и строки
+            var bbb = new string[] { "123", "sss" };
             var bbbb = "ffff";
 
-            var ss = new Tuple<int, string, char, string, ulong>(1,"",'s',"",2);//кортеж
-            Console.WriteLine(ss);//вывод всего и по ээлементам
+            var ss = new Tuple<int, string, char, string, ulong>(1,"",'s',"",2);
+            Console.WriteLine(ss);
 
             Console.WriteLine();
             Console.WriteLine(ss.Item1);
@@ -144,7 +143,7 @@ namespace Lab2//определение пространства имен
            for (int i = 0; i < l; i++)
                 sum += a[i];
            
-            return Tuple.Create<int, int, int, char>(a[l-1], a[0], sum, s[0]);//мин макс эл строки 
+            return Tuple.Create<int, int, int, char>(a[l-1], a[0], sum, s[0]);
         }
     }
 
