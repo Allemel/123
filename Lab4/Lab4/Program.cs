1f9e588bee6e6584f66dad0b9b5dae6e85819d26
Allemel <allemel@list.ru>
2017-11-15 23:17:50 +0300

    Lab5

--- Lab4/Lab4/Program.cs
+++ Lab4/Lab4/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -41,6 +42,8 @@ namespace Lab4
 
             Console.WriteLine("MAX: " + MathObject.Max(set2));
 
+            Console.WriteLine("Date is " + DateTime.Now);
+           
         }
     }
 }
